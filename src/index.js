1dd1548f26ef774e9c245b8f8d74d60eb396aedb
Esdras Clother <esdras.clother@outlook.com>
2021-09-01 13:14:28 -0600

    project start, file cleanup + font settings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+WebFont.load({
+  google: {
+    // Work+Sans:wght@400;500;700
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
+
+const Index = () => {
+  return (
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Index />, document.getElementById('root'));
