f7ed8254c1ca44a18e5753ac8d466b2c9a545160
Esdras Clother <esdras.clother@outlook.com>
2021-09-03 08:24:18 -0600

    creating the form layout to login

--- src/components/InicioSesion.js
+++ src/components/InicioSesion.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import styled from 'styled-components'
+
+import { Header, Titulo, ContenedorHeader } from '../elements/Header'
+import { Formulario, Input, ContenedorBoton } from '../elements/ElementosDeFormulario'
+import Boton from '../elements/Boton'
+import { ReactComponent as SvgLogin } from '../img/login.svg'
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 12.5rem; /* 100px */
+    margin-bottom: 1.25rem; /* 20px */
+`;
 
 const IniciSesion = () => {
     return (
-        <h1>Inicio de Sesion</h1>
+        <>
+            <Helmet>
+                <title>Iniciar Sesíon</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Iniciar Sesíon</Titulo>
+                    <div>
+                        <Boton to="/crear-cuenta">Registrarse</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg />
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electronico"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                />
+
+                <ContenedorBoton>
+                    <Boton as="button" primario type="submit">Iniciar Sesíon</Boton>
+                </ContenedorBoton>
+            </Formulario>
+        </>
     );
 }
 
