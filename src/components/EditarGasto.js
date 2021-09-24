bf58401ff71f4cfe79244a0e371d836fe797379e
Esdras Clother <esdras.clother@outlook.com>
2021-09-24 15:34:18 -0600

    creating the hook to obtain an expense per id.

--- src/components/EditarGasto.js
+++ src/components/EditarGasto.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useParams } from 'react-router-dom'
+
+import BtnRegresar from '../elements/BtnRegresar'
+import { Header, Titulo } from '../elements/Header'
+import BarraTotalGastado from './BarraTotalGastado'
+import FormularioGasto from './FormularioGasto'
+import useObtenerGasto from '../hooks/useObtenerGasto'
 
 const EditarGasto = () => {
+
+    const {id} = useParams();
+    const [gasto] = useObtenerGasto(id);
+    console.log(gasto);
+
     return (
-        <h1>Editar Gasto</h1>
+        <>
+            <Helmet>
+                <title>Editar Gasto</title>
+            </Helmet>
+
+            <Header>
+                <BtnRegresar />
+                <Titulo>Editar Gasto</Titulo>
+            </Header>
+
+            <FormularioGasto />
+
+            <BarraTotalGastado />
+        </>
     );
 }
 
