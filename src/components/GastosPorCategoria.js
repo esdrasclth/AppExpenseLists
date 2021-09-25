580dd8815fa4152849d4aea6bb9d00b835bc9199
Esdras Clother <esdras.clother@outlook.com>
2021-09-25 00:58:45 -0600

    creating expense logic by category of the month, previous category bar

--- src/components/GastosPorCategoria.js
+++ src/components/GastosPorCategoria.js
@@ -4,8 +4,12 @@ import { Helmet } from 'react-helmet'
 import BtnRegresar from '../elements/BtnRegresar'
 import { Header, Titulo } from '../elements/Header'
 import BarraTotalGastado from './BarraTotalGastado'
+import useObtenerGastosDelMesPorCategoria from '../hooks/useObtenerGastosDelMesPorCategoria'
 
 const GastosPorCategoria = () => {
+    const gastos = useObtenerGastosDelMesPorCategoria();
+    console.log(gastos)
+
     return (
         <>
             <Helmet>
